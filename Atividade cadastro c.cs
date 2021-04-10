da3e5ec7394697c4233164db57121666c6d77e28
Guilherme Moreira <guilherme.mocav@gmail.com>
2021-04-10 00:19:00 -0300

    Desenvolvimento de Sistemas

---
+++ Atividade cadastro c.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRIMEIRO_PROJETO_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cliente: Guilherme");
+            Console.ReadKey();
+            Console.WriteLine("Cidade São Paulo");
+            Console.ReadKey();
+            Console.WriteLine("Endereço: Rua Das Palmeiras, 79");
+            Console.ReadKey();
+            Console.WriteLine("Telefone: 11 9723827842");
+            Console.ReadKey();
+            Console.WriteLine("Idade: 22");
+            Console.ReadKey();
+        }
+    }()
+}
